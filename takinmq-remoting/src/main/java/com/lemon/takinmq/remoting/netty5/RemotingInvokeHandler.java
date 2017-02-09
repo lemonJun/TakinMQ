2ddb2459f83fcbd908d2c05e39566e7f5b249733
lemonJun <506526593@qq.com>
2017-02-09 11:10:42 +0800

    handler

--- takinmq-remoting/src/main/java/com/lemon/takinmq/remoting/netty5/RemotingInvokeHandler.java
+++ takinmq-remoting/src/main/java/com/lemon/takinmq/remoting/netty5/RemotingInvokeHandler.java
@@ -1,8 +1,11 @@
 package com.lemon.takinmq.remoting.netty5;
 
+import java.lang.reflect.Method;
+
 import org.apache.log4j.Logger;
 
 import com.alibaba.fastjson.JSON;
+import com.lemon.takinmq.common.util.SerializeUtil;
 import com.lemon.takinmq.remoting.GlobalContext;
 
 import io.netty.channel.ChannelHandlerAdapter;
@@ -33,41 +36,30 @@ public class RemotingInvokeHandler extends ChannelHandlerAdapter {
             logger.info(JSON.toJSONString(msg));
             msg.setResultJson("hello world");
             ctx.writeAndFlush(msg);
-            return;
-            //        if (msg.getType() == MessageType.REMOTING_INVOKE.value()) {
-            //            //            String clazzName = msg.getClazz();
-            //            //            String methodName = msg.getMethod();
-            //            //            Object[] args = msg.getArgs();
-            //            //            //            ClassPool pool = ClassPool.getDefault();
-            //            //            //            Class clazz = pool.get(clazzStr).toClass();
-            //            //            Class c[] = null;
-            //            //            if (args != null) {//存在
-            //            //                int len = args.length;
-            //            //                c = new Class[len];
-            //            //                for (int i = 0; i < len; ++i) {
-            //            //                    c[i] = args[i].getClass();
-            //            //                }
-            //            //            }
-            //            //            Class clazz = Class.forName(clazzName);
-            //            //            Method method = clazz.getDeclaredMethod(methodName, c);
-            //            //            //            Object impl = GuiceDI.getInstance(clazz);
-            //            //            Object impl = clazz.newInstance();
-            //            //
-            //            //            Object result = method.invoke(impl, args);
-            //            //            if (!method.getReturnType().getName().equals("void")) {
-            //            //                msg.setResultJson(SerializeUtil.jsonSerialize(result));
-            //            //            }
-            //            msg.setResultJson("hello world");
-            //            ctx.writeAndFlush(msg);
-            //        } else {
-            //            msg.setResultJson("hello world");
-            //            ctx.writeAndFlush(msg);
-            //            /**
-            //             * 继续放给下一个handler处理
-            //             * 如查后面已经没有handler了  不能用这个方法  否则会报 Discarded inbound message  错误
-            //             */
-            //            //            ctx.fireChannelRead(obj);//
-            //        }
+            String clazzName = msg.getClazz();
+            String methodName = msg.getMethod();
+            Object[] args = msg.getArgs();
+            //            ClassPool pool = ClassPool.getDefault();
+            //            Class clazz = pool.get(clazzStr).toClass();
+            Class c[] = null;
+            if (args != null) {//存在
+                int len = args.length;
+                c = new Class[len];
+                for (int i = 0; i < len; ++i) {
+                    c[i] = args[i].getClass();
+                }
+            }
+            Class clazz = Class.forName(clazzName);
+            Method method = clazz.getDeclaredMethod(methodName, c);
+            //            Object impl = GuiceDI.getInstance(clazz);
+            Object impl = clazz.newInstance();
+
+            Object result = method.invoke(impl, args);
+            if (!method.getReturnType().getName().equals("void")) {
+                msg.setResultJson(SerializeUtil.jsonSerialize(result));
+            }
+            msg.setResultJson("hello world");
+            ctx.writeAndFlush(msg);
         } finally {
             GlobalContext.getSingleton().removeThreadLocal();
         }

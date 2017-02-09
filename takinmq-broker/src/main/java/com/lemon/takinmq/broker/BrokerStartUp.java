614ed1ba7085bec132aaed82c7b109caeac99541
lemonJun <506526593@qq.com>
2017-02-09 17:50:54 +0800

    实现broker的register

---
+++ takinmq-broker/src/main/java/com/lemon/takinmq/broker/BrokerStartUp.java
@@ -0,0 +1,34 @@
+package com.lemon.takinmq.broker;
+
+import com.lemon.takinmq.common.ImoduleService;
+
+/**
+ * broker启动类
+ *
+ * @author WangYazhou
+ * @date  2017年2月9日 下午5:41:18
+ * @see
+ */
+public class BrokerStartUp implements ImoduleService {
+
+    @Override
+    public void env() throws Exception {
+
+    }
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
+    public void start() throws Exception {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+}

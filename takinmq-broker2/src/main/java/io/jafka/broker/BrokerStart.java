e46957ae7d8df702038b25112fd70af7ff256d99
lemonJun <506526593@qq.com>
2017-04-20 12:06:25 +0800

    up

--- takinmq-broker2/src/main/java/io/jafka/broker/BrokerStart.java
+++ takinmq-broker2/src/main/java/io/jafka/broker/BrokerStart.java
@@ -1,14 +1,28 @@
 package io.jafka.broker;
 
+import java.io.File;
+
+import com.takin.rpc.server.GuiceDI;
 import com.takin.rpc.server.RPCServer;
 
 public class BrokerStart {
 
+    private static final RPCServer server = new RPCServer();
+
     public static void main(String[] args) {
         try {
-            new RPCServer().init(new String[] {}, false);
+            server.init(new String[] {}, false);
+            server.start();
+            initBroker();
         } catch (Exception e) {
             e.printStackTrace();
+            System.exit(-1);
         }
     }
+
+    public static void initBroker() {
+        BrokerConfig config = GuiceDI.getInstance(BrokerConfig.class);
+        config.init(server.getContext().getConfigPath() + File.separator + "broker.properties");
+    }
+
 }
